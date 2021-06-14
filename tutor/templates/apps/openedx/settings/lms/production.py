4e0aefbfe91393bc872f38ae9ac92dd8004d7b33
Артем Маршалкин <>
2021-06-14 23:22:00 +0300

    Enable edx-enterprise

--- tutor/templates/apps/openedx/settings/lms/production.py
+++ tutor/templates/apps/openedx/settings/lms/production.py
@@ -26,4 +26,25 @@ DCS_SESSION_COOKIE_SAMESITE = "Lax"
 # Required to display all courses on start page
 SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
 
+# Enable edx-enterprise features
+LMS_ROOT_URL = ENV_TOKENS.get("LMS_ROOT_URL")
+
+SYSTEM_WIDE_ROLE_CLASSES.append('enterprise.SystemWideEnterpriseUserRoleAssignment')
+ENTERPRISE_API_URL = f"{ LMS_ROOT_URL }/enterprise/api/v1/"
+
+ENTERPRISE_LEARNER_PORTAL_NETLOC = 'learner.lms.knotta.ru'
+ENTERPRISE_LEARNER_PORTAL_BASE_URL = 'https://' + ENTERPRISE_LEARNER_PORTAL_NETLOC
+
+ENTERPRISE_ADMIN_PORTAL_NETLOC = 'enterprise.lms.knotta.ru'
+ENTERPRISE_ADMIN_PORTAL_BASE_URL = 'https://' + ENTERPRISE_ADMIN_PORTAL_NETLOC
+
+ENTERPRISE_CATALOG_INTERNAL_ROOT_URL = 'http://enterprise.catalog.app:18160'
+
+LOGIN_REDIRECT_WHITELIST.append(ENTERPRISE_LEARNER_PORTAL_NETLOC)
+LOGIN_REDIRECT_WHITELIST.append(ENTERPRISE_ADMIN_PORTAL_NETLOC)
+
+FEATURES.update({"ENABLE_ENTERPRISE_INTEGRATION": True})
+
+MKTG_URLS.update({'ENTERPRISE': '/enterprise'})
+
 {{ patch("openedx-lms-production-settings") }}
